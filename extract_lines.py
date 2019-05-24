4c46e9d16f35a4dd63f512fa3ab83d3cbcf7a05a
Tim Flaspoehler <tflaspoehler@Tims-MBP.attlocal.net>
2019-05-24 10:32:48 -0400

    removed ignored files

--- extract_lines.py
+++
@@ -1,6 +0,0 @@
-lines = open('imcdates.html', 'r').readlines()
-
-title_lines = [i for i in range(0, len(lines)) if 'flexible-content-market-show-title' in lines[i]]
-
-for i in title_lines:
-	print lines[i]
